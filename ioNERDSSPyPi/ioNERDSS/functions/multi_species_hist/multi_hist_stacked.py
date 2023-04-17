42d2c9d76b014f36d3e771c085aaa3e36b4cf186
IanHP <129088911+IanHP@users.noreply.github.com>
2023-04-17 17:05:46 -0400

    optimized multi_hist_stacked

--- ioNERDSSPyPi/ioNERDSS/functions/multi_species_hist/multi_hist_stacked.py
+++ ioNERDSSPyPi/ioNERDSS/functions/multi_species_hist/multi_hist_stacked.py
@@ -6,17 +6,17 @@ from .read_multi_hist import read_multi_hist
 def multi_hist_stacked(FileName: str, FileNum: int, InitialTime: float, FinalTime: float,
                        SpeciesList: list, xAxis: str, DivideSpecies: str, DivideSize: int,
                        BarSize: int = 1, ExcludeSize: int = 0, ShowFig: bool = True, SaveFig: bool = False):
-    """Creates a histogram from histogram.dat (multi-species) that shows the average number of complex species of a certain size.
+    """Creates a stacked histogram from histogram.dat (multi-species) that shows the average number of complex species of each protein composition over the whole sim. 
 
     Args:
         FileName (str): Path to the histogram.dat file
         FileNum (int): Number of the total input files (file names should be [fileName]_1,[fileName]_2,...)
         InitialTime (float): The starting time. Must not be smaller / larger then times in file.
         FinalTime (float): The ending time. Must not be smaller / larger then times in file.
-        SpeciesList (list): The names of the species you want to examine. Should be in the .dat file.
         xAxis (str): Species shown on X-axis.
         DivideSpecies (str): The name of the species that will be seperated by size.
         DivideSize (int): The value that separates the size of dissociate complexes. (only changes color of graph)
+        SpeciesList (list): The names of the species you want to examine. Should be in the .dat file.
         BarSize (int, optional): The size of each data bar in the X-dimension. Defaults to 1.
         ExcludeSize (int, optional): Monomers in the complex that are smaller or equal to this number will not be included. 
         ShowFig (bool, optional): If the plot is shown. Defaults to True.
@@ -24,188 +24,126 @@ def multi_hist_stacked(FileName: str, FileNum: int, InitialTime: float, FinalTim
     Returns:
         Histogram. X-axis = size of selected species, Y-axis = average number of each corresponds.
     """
+
     file_name_head = FileName.split('.')[0]
     file_name_tail = FileName.split('.')[1]
     above_list = []
     equal_list = []
     below_list = []
-    size_list = []
-    for i in range(1, FileNum+1):
-        temp_file_name = file_name_head + '_' + str(i) + '.' + file_name_tail
+
+    #create species list. (Other species = 0:-1, devide species = -1).
+    if xAxis == 'tot':
+        SpeciesList.remove(DivideSpecies)
+        SpeciesList.append(DivideSpecies)
+    else:
+        SpeciesList = []
+        SpeciesList.append(xAxis)
+        SpeciesList.append(DivideSpecies)
+
+    #for each file
+    for histogram_file_number in range(1, FileNum+1):
+        #determining file name (if there are multiple or none)
         if FileNum == 1:
             temp_file_name = FileName
-        total_size_list = []
-        total_above_list = []
-        total_equal_list = []
-        total_below_list = []
+        else:
+            temp_file_name = file_name_head + '_' + str(histogram_file_number) + '.' + file_name_tail
+
+        total_above_dict = {}
+        total_equal_dict = {}
+        total_below_dict = {}
+
+        #read histogram file
         hist_list = read_multi_hist(temp_file_name, SpeciesList)
+
         data_count = 0
-        for j in range(len(hist_list)):
-            if hist_list[j] != []:
-                time = hist_list[j][0]
+
+        #for every time step
+        for time_step in hist_list:
+            if time_step != []:
+                time = time_step[0]
                 if InitialTime <= time <= FinalTime:
                     data_count += 1
-                    for k in range(len(hist_list[j])):
-                        if k != 0:
-                            if xAxis == 'tot' and DivideSpecies in SpeciesList:
-                                total_size = sum(hist_list[j][k][0:-1])
-                                divide_index = SpeciesList.index(DivideSpecies)
-                                divide_spe_size = hist_list[j][k][divide_index]
-                                if total_size >= ExcludeSize:
-                                    if total_size not in total_size_list:
-                                        total_size_list.append(total_size)
-                                        if divide_spe_size > DivideSize:
-                                            total_above_list.append(
-                                                hist_list[j][k][-1])
-                                            total_equal_list.append(0.0)
-                                            total_below_list.append(0.0)
-                                        elif divide_spe_size == DivideSize:
-                                            total_equal_list.append(
-                                                hist_list[j][k][-1])
-                                            total_above_list.append(0.0)
-                                            total_below_list.append(0.0)
-                                        else:
-                                            total_below_list.append(
-                                                hist_list[j][k][-1])
-                                            total_above_list.append(0.0)
-                                            total_equal_list.append(0.0)
-                                    else:
-                                        if divide_spe_size > DivideSize:
-                                            index = total_size_list.index(
-                                                total_size)
-                                            total_above_list[index] += hist_list[j][k][-1]
-                                        elif divide_spe_size == DivideSize:
-                                            index = total_size_list.index(
-                                                total_size)
-                                            total_equal_list[index] += hist_list[j][k][-1]
-                                        else:
-                                            index = total_size_list.index(
-                                                total_size)
-                                            total_below_list[index] += hist_list[j][k][-1]
-                            elif xAxis in SpeciesList and DivideSpecies in SpeciesList:
-                                name_index = SpeciesList.index(xAxis)
-                                total_size = hist_list[j][k][name_index]
-                                divide_index = SpeciesList.index(DivideSpecies)
-                                divide_spe_size = hist_list[j][k][divide_index]
-                                if total_size >= ExcludeSize:
-                                    if total_size not in total_size_list:
-                                        total_size_list.append(total_size)
-                                        if divide_spe_size > DivideSize:
-                                            total_above_list.append(
-                                                hist_list[j][k][-1])
-                                            total_equal_list.append(0.0)
-                                            total_below_list.append(0.0)
-                                        elif divide_spe_size == DivideSize:
-                                            total_equal_list.append(
-                                                hist_list[j][k][-1])
-                                            total_above_list.append(0.0)
-                                            total_below_list.append(0.0)
-                                        else:
-                                            total_below_list.append(
-                                                hist_list[j][k][-1])
-                                            total_above_list.append(0.0)
-                                            total_equal_list.append(0.0)
-                                    else:
-                                        if divide_spe_size > DivideSize:
-                                            index = total_size_list.index(
-                                                total_size)
-                                            total_above_list[index] += hist_list[j][k][-1]
-                                        elif divide_spe_size == DivideSize:
-                                            index = total_size_list.index(
-                                                total_size)
-                                            total_equal_list[index] += hist_list[j][k][-1]
-                                        else:
-                                            index = total_size_list.index(
-                                                total_size)
-                                            total_below_list[index] += hist_list[j][k][-1]
+
+                    #for every protein complex
+                    for protein_complex in time_step[1:]:
+                        
+                        #if number of selected proteisn in the complex
+                        if xAxis == 'tot' and DivideSpecies in SpeciesList:
+                            total_size = sum(protein_complex[0:-1])
+                        elif xAxis in SpeciesList and DivideSpecies in SpeciesList:
+                            total_size = protein_complex[0]
+                        
+                        if total_size >= ExcludeSize:
+                            divide_spe_size = protein_complex[-2]
+                                
+                            #check if there is already a key for a protein of this size. If there isn't add it
+                            if not total_size in total_above_dict:
+                                total_above_dict[total_size] = 0
+                                total_equal_dict[total_size] = 0
+                                total_below_dict[total_size] = 0
+
+                            #if complex size not included, initlize this size, and add the total number of this kind of complex to lists of above/below
+                            if divide_spe_size > DivideSize:
+                                total_above_dict[total_size] += (protein_complex[-1])
+                            elif divide_spe_size == DivideSize:
+                                total_equal_dict[total_size] += (protein_complex[-1])
                             else:
-                                print('xAxis or DivideSpecies not in SpeciesList!')
-                                return 0
-        total_above_list = np.array(total_above_list)/data_count
-        total_equal_list = np.array(total_equal_list)/data_count
-        total_below_list = np.array(total_below_list)/data_count
-        if len(total_size_list) != 0:
-            total_size_list_sorted = np.arange(1, max(total_size_list)+1, 1)
-        else:
-            total_size_list_sorted = np.array([])
-        total_above_list_sorted = []
-        for i in total_size_list_sorted:
-            if i in total_size_list:
-                index = total_size_list.index(i)
-                total_above_list_sorted.append(total_above_list[index])
-            else:
-                total_above_list_sorted.append(0.0)
-        total_equal_list_sorted = []
-        for i in total_size_list_sorted:
-            if i in total_size_list:
-                index = total_size_list.index(i)
-                total_equal_list_sorted.append(total_equal_list[index])
-            else:
-                total_equal_list_sorted.append(0.0)
-        total_below_list_sorted = []
-        for i in total_size_list_sorted:
-            if i in total_size_list:
-                index = total_size_list.index(i)
-                total_below_list_sorted.append(total_below_list[index])
-            else:
-                total_below_list_sorted.append(0.0)
-        size_list.append(total_size_list_sorted)
-        above_list.append(total_above_list_sorted)
-        equal_list.append(total_equal_list_sorted)
-        below_list.append(total_below_list_sorted)
+                                total_below_dict[total_size] += (protein_complex[-1])
+
+        #devide each protein by # of timesteps to get average
+        for dict in (total_above_dict,total_below_dict,total_equal_dict):
+            for key in dict:
+                dict[key] /= data_count
+
+
+        #add dictionaries to main lists
+        above_list.append(total_above_dict)
+        equal_list.append(total_equal_dict)
+        below_list.append(total_below_dict)
+    
+    #find max size
     max_size = 0
-    for i in size_list:
-        if max_size < len(i):
-            max_size = len(i)
-            n_list = i
-    above_list_filled = np.zeros([FileNum, max_size])
-    above_list_arr = np.array([])
-    for i in range(len(above_list)):
-        for j in range(len(above_list[i])):
-            above_list_filled[i][j] += above_list[i][j]
-    equal_list_filled = np.zeros([FileNum, max_size])
-    equal_list_arr = np.array([])
-    for i in range(len(equal_list)):
-        for j in range(len(equal_list[i])):
-            equal_list_filled[i][j] += equal_list[i][j]
-    below_list_filled = np.zeros([FileNum, max_size])
-    below_list_arr = np.array([])
-    for i in range(len(below_list)):
-        for j in range(len(below_list[i])):
-            below_list_filled[i][j] += below_list[i][j]
-    above_list_rev = []
-    for i in range(len(above_list_filled[0])):
-        temp = []
-        for j in range(len(above_list_filled)):
-            temp.append(above_list_filled[j][i])
-        above_list_rev.append(temp)
-    equal_list_rev = []
-    for i in range(len(equal_list_filled[0])):
-        temp = []
-        for j in range(len(equal_list_filled)):
-            temp.append(equal_list_filled[j][i])
-        equal_list_rev.append(temp)
-    below_list_rev = []
-    for i in range(len(below_list_filled[0])):
-        temp = []
-        for j in range(len(below_list_filled)):
-            temp.append(below_list_filled[j][i])
-        below_list_rev.append(temp)
+    for key in total_above_dict:
+        if max_size < int(key):
+            max_size = int(key)
+        n_list = range(0,int(key+1))
+
+    #add dictionary values to filled lists and transposes them for prep for mean
+    above_list_filled = np.zeros([max_size+1,FileNum])
+    for indexX,dict in enumerate(above_list):
+        for key in dict:
+            above_list_filled[int(key)][indexX] += dict[key]
+
+    equal_list_filled = np.zeros([max_size+1,FileNum])
+    for indexX,dict in enumerate(equal_list):
+        for key in dict:
+            equal_list_filled[int(key)][indexX] += dict[key]
+    
+    below_list_filled = np.zeros([max_size+1,FileNum])
+    for indexX,dict in enumerate(below_list):
+        for key in dict:
+            below_list_filled[int(key)][indexX] += dict[key]
+
+    #is it mean time?
     mean_above = []
     std_above = []
     mean_equal = []
     std_equal = []
     mean_below = []
     std_below = []
-    for i in above_list_rev:
-        mean_above.append(np.nanmean(i))
-        std_above.append(np.nanstd(i))
-    for i in equal_list_rev:
-        mean_equal.append(np.nanmean(i))
-        std_equal.append(np.nanstd(i))
-    for i in below_list_rev:
-        mean_below.append(np.nanmean(i))
-        std_below.append(np.nanstd(i))
+    for protein_size in above_list_filled:
+        mean_above.append(np.nanmean(protein_size))
+        std_above.append(np.nanstd(protein_size))
+    for protein_size in equal_list_filled:
+        mean_equal.append(np.nanmean(protein_size))
+        std_equal.append(np.nanstd(protein_size))
+    for protein_size in below_list_filled:
+        mean_below.append(np.nanmean(protein_size))
+        std_below.append(np.nanstd(protein_size))
+    
+
+    
+    #will combine means together if bar size is too small
     mean_above_ = []
     mean_equal_ = []
     mean_below_ = []
@@ -228,15 +166,7 @@ def multi_hist_stacked(FileName: str, FileNum: int, InitialTime: float, FinalTim
         temp_std_equal += std_equal[i]
         temp_std_below += std_below[i]
         bar_size_count += 1
-        if i+1 == len(mean_above):
-            mean_above_.append(temp_mean_above)
-            mean_equal_.append(temp_mean_equal)
-            mean_below_.append(temp_mean_below)
-            std_above_.append(temp_std_above)
-            std_equal_.append(temp_std_equal)
-            std_below_.append(temp_std_below)
-            n_list_.append(n_list[i])
-        elif bar_size_count >= BarSize:
+        if bar_size_count >= BarSize:
             mean_above_.append(temp_mean_above)
             mean_equal_.append(temp_mean_equal)
             mean_below_.append(temp_mean_below)
@@ -251,6 +181,14 @@ def multi_hist_stacked(FileName: str, FileNum: int, InitialTime: float, FinalTim
             temp_std_equal = 0
             temp_std_below = 0
             bar_size_count = 0
+    
+    mean_above_.append(temp_mean_above)
+    mean_equal_.append(temp_mean_equal)
+    mean_below_.append(temp_mean_below)
+    std_above_.append(temp_std_above)
+    std_equal_.append(temp_std_equal)
+    std_below_.append(temp_std_below)
+    n_list_.append(n_list[i])
     mean_above_ = np.array(mean_above_)
     mean_equal_ = np.array(mean_equal_)
     mean_below_ = np.array(mean_below_)
@@ -258,6 +196,8 @@ def multi_hist_stacked(FileName: str, FileNum: int, InitialTime: float, FinalTim
     std_equal_ = np.array(std_equal_)
     std_below_ = np.array(std_below_)
     n_list_ = np.array(n_list_)
+    
+    #show figure!
     if ShowFig:
         if DivideSize != 0:
             below_label = DivideSpecies + '<' + str(DivideSize)
