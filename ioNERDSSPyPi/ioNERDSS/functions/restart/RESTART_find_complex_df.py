b2580c84420c23050ed6e6539210482082fcc027
IanHP <129088911+IanHP@users.noreply.github.com>
2023-04-18 16:39:03 -0400

    slight documenting change

--- ioNERDSSPyPi/ioNERDSS/functions/restart/RESTART_find_complex_df.py
+++ ioNERDSSPyPi/ioNERDSS/functions/restart/RESTART_find_complex_df.py
@@ -16,56 +16,25 @@ def RESTART_find_complex_df(complex_df, num_lst):
     protein_remain = [] #list of proteins in the complexes (what is returned)
     protein_complex_hits = 0 #how many protein complexes have the correct number
     
-
-    #run through each row of the dataframe
+    #each row of the dataframe
     for row in list_of_rows:
-        #for every row/complex_protein in the dataframe
 
+        #if row/protein complex has the correct number of each protein type
         if row[0:-1] == num_lst: 
-            #if row/complex_protein has the correct number of proteins in each structure
+            
             protein_complex_hits += 1
-
-            list_of_protein_nums = row[-1].strip('[').strip(']').split(',') #this is a list of the proteins in this complex
+            list_of_protein_nums = row[-1].strip('[').strip(']').split(',') #gets the list of the specific proteins in this complex
+            
+            #add every protein num that is in that complex
             for protein_num in list_of_protein_nums:
-                #add every protein num that is in that complex
                 protein_remain.append(int(protein_num))
     
-    
+    #print success / failure
     if protein_complex_hits == 0:
         print('No complexes where found with the specific number of protein types.')
     else:
         print(f'Found {protein_complex_hits} protein complexes!')
 
     return protein_remain
-
-"""#creates a new column in complex_df that stores a list of the number of each protein type in that complex
-    complex_df['Num_List'] = '' #adds num_list column to df
-    for row in range(complex_df.shape[0]):
-        #for each row in the dataframe
-
-        lst = []
-        for column in range(complex_df.shape[1]-2):
-            #for each column in this row (not including protein_num,num_list)
-
-            lst.append(complex_df.iloc[row, column])
-
-        complex_df.loc[row, 'Num_List'] = str(lst)
-    input(f'{complex_df}')
-
-    #finds proteins that have a size equal to inputted number
-    protein_remain = [] #list of proteins with size = input number
-    for row in range(complex_df.shape[0]):
-        #for each row
-
-        input(f"{complex_df.loc[row, 'Num_List']} == {str(num_lst)}")
-        if complex_df.loc[row, 'Num_List'] == str(num_lst):
-            #if that rows num_list = inputted
-
-            protein_list = complex_df.loc[row, 'Protein_Num']
-            protein_list = protein_list.strip('[').strip(']').split(',')
-            for protein in protein_list:
-                protein_remain.append(int(protein))
-            input(f"{protein_remain}")
-    return protein_remain"""
 
 
