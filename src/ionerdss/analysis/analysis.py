29e50bf2d39b6ff604747e12d1aebf2ac55774ea
Jonathan Fischer <jfisch27@jhu.edu>
2025-07-18 13:23:41 -0400

    Use src layout and rename Analysis class

--- src/ionerdss/analysis/analysis.py
+++ src/ionerdss/analysis/analysis.py
@@ -11,7 +11,7 @@ from .plotting import PlotConfigure
 from .legacy import LegacyPlotInterface
 
 
-class Analyzer:
+class Analysis:
     """
     Main interface for analyzing and visualizing NERDSS simulation results.
     
